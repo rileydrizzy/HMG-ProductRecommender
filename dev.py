08b1b9023dc12546c60d6bdc92a936a72016d645
Ipadeola Ladipo Ezekiel <rileydrizzy@hotmail.com>
2023-11-05 08:40:36 +0000

    backup

--- dev.py
+++ dev.py
@@ -6,8 +6,6 @@ COMMAND = [
     "-c",
     "h-and-m-personalized-fashion-recommendations",
 ]
-#COMMAND[6] =  'images/'  #'images/010/0108775015.jpg' #"sample_submission.csv"
-#COMMAND[-1] = 'dev_data'
 r = subprocess.run(COMMAND, check= False, capture_output= True, text =True)
 
 with open('new.txt', 'w', encoding= 'utf-8') as file:
